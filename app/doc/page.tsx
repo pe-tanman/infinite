458dc1f764d1b4d6c8deb9d0f43d2617e3e3a28e
pe_tanman <pe.tanman.12@gmail.com>
2025-06-28 18:18:48 -0700

    improve the design of default components

--- app/doc/page.tsx
+++ app/doc/page.tsx
@@ -4,26 +4,42 @@ import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote'
 import React from 'react';
 import Props from '@/components/GetProps';
 
-
-const mdxString: string = `
-# About Us
-Welcome! We are a passionate team dedicated to building incredible tools that empower developers and content creators
-alike. Our mission is to make complex documentation simple, beautiful, and maintainable.
-
-\`\`\`javascript
-// This is a standard markdown code fence
-function welcome() {
-    console.log("Hello from a Markdoc-powered page!");
-}
-welcome();
-\`\`\`
-`;
-
 const overrideComponents = {
     h1: (props: React.HTMLAttributes<HTMLHeadingElement>) => (
         <h1
             {...props}
-            className="text-4xl font-bold text-gray-800 mb-6 border-b-2 border-gray-300 pb-2"
+            className="text-4xl font-bold mb-6 border-b-2 border-gray-300 pb-2"
+        />
+    ),
+    h2: (props: React.HTMLAttributes<HTMLHeadingElement>) => (
+        <h2
+            {...props}
+            className="text-2xl font-bold mb-4 mt-6"
+        />
+    ),
+    h3: (props: React.HTMLAttributes<HTMLHeadingElement>) => (
+        <h3
+            {...props}
+            className="text-xl font-semibold mb-2 mt-4 text-gray-700"
+        />
+    ),
+    ul: (props: React.HTMLAttributes<HTMLUListElement>) => (
+        <ul
+            {...props}
+            className="list-disc list-inside pl-6 mb-2"
+        />
+    ),
+    ol: (props: React.HTMLAttributes<HTMLOListElement>) => (
+        <ol
+            {...props}
+            className="list-decimal list-inside pl-6 mb-2"
+        >
+        </ol>
+    ),
+    blockquote: (props: React.HTMLAttributes<HTMLElement>) => (
+        <blockquote
+            {...props}
+            className="border-l-4  border-gray-400 pl-4 italic text-gray-800 my-4py-2"
         />
     ),
 };
@@ -41,7 +57,7 @@ export default function TestPage() {
 
     if (!props) return <div>Loading...</div>;
     return (
-        <div className="bg-white p-12 rounded-2xl shadow-md max-w-3xl mx-auto my-10">
+        <div className="bg-white p-12 rounded-2xl max-w-3xl mx-auto my-10 shadow-sm">
             <MDXRemote {...props.source} components={overrideComponents} />
         </div>
     );
--- components/GetProps.tsx
+++ components/GetProps.tsx
@@ -8,17 +8,38 @@ const options = {
 export default async function Props() {
     // MDX text - can be from a local file, database, anywhere
     const source = `
-    Some **mdx** text, with a component
+    # About MDX
 
-    \`\`\`javascript
-    // This is a standard markdown code fence
-    function welcome() {
-        console.log("Hello from a Markdoc-powered page!");
-    }
-    welcome();
-    \`\`\`
-    `;
-    
+    ## Welcome to MDX
+
+    ### Heading 3
+
+    > blockquote: MDX is a powerful way to write JSX in Markdown.
+
+    A backslash\
+before a line break…
+
+
+Some *asterisks* for emphasis.
+
+Three asterisks for a thematic break:
+
+***
+
+* asterisks for unordered items
+1. decimals and a dot for ordered items
+
+Welcome! We are a passionate team dedicated to building **incredible** tools that empower developers and content creators alike. Our mission is to make complex documentation simple, beautiful, and maintainable.
+
+\`\`\`javascript
+// This is a standard markdown code fence
+function welcome() {
+    console.log("Hello from a Markdoc-powered page!");
+}
+welcome();
+\`\`\`
+`;
+
     const mdxSource = await serialize(source, {mdxOptions: { rehypePlugins: [[rehypePrettyCode, options]] }});
     return { props: { source: mdxSource }}
 }
